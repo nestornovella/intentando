e3b409a804012e961b148e7b11a1b64bd13270fb
Nestor Novella <importarnorth@gmail.com>
2023-01-24 21:23:38 -0300

    end

--- client/src/functions/apiInfo.js
+++ client/src/functions/apiInfo.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 
-
+const apiRailwai = "https://portfolio-production-77df.up.railway.app/"
 
 export async function sendData(data){
-   await axios.post("http://localhost:3001", data)
+   await axios.post(apiRailwai, data)
    return "enviado"
 }
 
