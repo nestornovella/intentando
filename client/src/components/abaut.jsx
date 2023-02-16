5d7d930868426f3c6bbafd619674c03f91c69a3b
Nestor Novella <importarnorth@gmail.com>
2023-02-16 12:27:12 -0300

    changes a fixes

--- client/src/components/abaut.jsx
+++ client/src/components/abaut.jsx
@@ -4,58 +4,62 @@ import { useEffect, useRef } from "react"
 import Typed from "typed.js"
 
 
-export default function Abaut (){
-    
+export default function Abaut() {
+
     const el = useRef(null)
 
     const arr = ["I love challenges, teamwork and constant learning.",
-     "In my spare time I play video games with my son, or do outdoor activities.",
-      "I love the back end, although I don't dislike the front.",
-       "I am a proactive and self-taught person",
-       "I am constantly learning, and I love programming!💙"]
-        function typear (strArr){
-            const typed = new Typed(el.current, {
-                strings:strArr.map(e=> `<strong>${e}</strong>`),
-                startDelay:300,
-                typeSpeed:20,
-                smartBackspace:false,
-                backDelay:2000,
-                backSpeed:1,
-                showCursor:true,
-                loop:false
-                
-            })
-            return typed
-        }
+        "In my spare time I play video games with my son, or do outdoor activities.",
+        "I love the back end, although I don't dislike the front.",
+        "I am a proactive and self-taught person",
+        "I am constantly learning, and I love programming!💙"]
+    function typear(strArr) {
+        const typed = new Typed(el.current, {
+            strings: strArr.map(e => `<strong>${e}</strong>`),
+            startDelay: 300,
+            typeSpeed: 20,
+            smartBackspace: false,
+            backDelay: 2000,
+            backSpeed: 1,
+            showCursor: true,
+            loop: false
+
+        })
+        return typed
+    }
     let start = false
 
 
 
-    const cargarImagen = (entradas, observador)=>{
+    const cargarImagen = (entradas, observador) => {
         entradas.forEach(entrada => {
-            if(entrada.isIntersecting){entrada.target.className = Styles.visible; start === false  && typear(arr); start = true}
-            else{entrada.target.className = Styles.izquierda; }
+            if (entrada.isIntersecting) { entrada.target.className = Styles.visible; start === false && typear(arr); start = true }
+            else { entrada.target.className = Styles.izquierda; }
         })
     }
 
-    
-    useEffect(()=>{
-        const observador = new IntersectionObserver(cargarImagen,{
-            root:null,
-            rootMargin:"0px 0px 0px 0px",
-            threshold:0.2
+
+    useEffect(() => {
+        const observador = new IntersectionObserver(cargarImagen, {
+            root: null,
+            rootMargin: "0px 0px 0px 0px",
+            threshold: 0.2
         })
         observador.observe(document.getElementById("prueba"))
-      
+
     })
-    return(
+    return (
         <div id="Abaut" className={Styles.container}>
             <div>
-                <img width={350}  id="prueba"  src={imgAbaut} alt="" />
+                <img id="prueba" src={imgAbaut} alt="" />
             </div>
             <div className={Styles.abaut}>
-                <h1 className={Styles.abaut}>About me...</h1>
-                <p><span ref={el}></span></p>
+                <div className={Styles.abautBox1}>
+                    <h1>About me...</h1>
+                </div>
+                <div className={Styles.aboutBox2}>
+                    <p><span ref={el}></span></p>
+                </div>
             </div>
         </div>
     )
