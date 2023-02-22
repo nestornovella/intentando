d001ea659c8831a666cc8ea134489f308a203190
Nestor Novella <importarnorth@gmail.com>
2023-02-22 19:53:21 -0300

    dbChanges

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const { Sequelize } = require("sequelize")
 const formModel = require("./src/models/Form")
 const visitedModel = require("./src/models/Visits")
-const sequelize = new Sequelize("postgresql://postgres:6vtgHlWDPfsKjOpTTLCx@containers-us-west-189.railway.app:7061/railway", {
+const sequelize = new Sequelize("postgresql://postgres:27tH9aSe3en6Jmu6TOBn@containers-us-west-126.railway.app:5796/railway", {
     dialect: 'postgres',
     protocol: 'postgres',
     logging: false,
